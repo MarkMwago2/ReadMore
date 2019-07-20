202dc16f13f5d8bb9085d3805dcc565538de60d0
MarkMwago2 <markmwago@gmail.com>
2019-07-20 12:52:21 +0300

    add store image to books activity

--- app/src/main/java/com/example/readmore/MainActivity.java
+++ app/src/main/java/com/example/readmore/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.readmore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindBooksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindBooksButton = (Button) findViewById(R.id.buyBooksbutton);
+        mFindBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BooksActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
