28ce8179853f274cb2962601139793d89b6428d8
MarkMwago2 <markmwago@gmail.com>
2019-07-21 23:20:04 +0300

    implement custom fonts in the app

--- app/src/main/java/com/example/readmore/MainActivity.java
+++ app/src/main/java/com/example/readmore/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.readmore;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindBooksButton;
     private Button mBookstoreButton;
+    private TextView mAppNameTextView;
 
 
     @Override
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mBookstoreButton = (Button) findViewById(R.id.Bookstorebutton);
         mFindBooksButton = (Button) findViewById(R.id.buyBooksbutton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+
 
         mBookstoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
