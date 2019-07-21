02f573a658672663b9024d9811677a9d1d620235
MarkMwago2 <markmwago@gmail.com>
2019-07-21 15:46:29 +0300

    implement search books EditText

--- app/src/main/java/com/example/readmore/MainActivity.java
+++ app/src/main/java/com/example/readmore/MainActivity.java
@@ -5,23 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindBooksButton;
+    private EditText msearchbooksEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        msearchbooksEditText = (EditText) findViewById(R.id.searchbooksEditText);
         mFindBooksButton = (Button) findViewById(R.id.buyBooksbutton);
         mFindBooksButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, BooksActivity.class);
-                startActivity(intent);
-            }
-        });
+
+                @Override
+                public void onClick(View v){
+                    Intent intent = new Intent(MainActivity.this, BooksActivity.class);
+                    String searchbooks = msearchbooksEditText.getText().toString();
+                    intent.putExtra("search", searchbooks);
+                    startActivity(intent);
+                }
+            });
+        }
     }
-}
+
+
+
