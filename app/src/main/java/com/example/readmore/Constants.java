5acb82d25fbd079042dbd2c2af5f0e5fef9c1239
MarkMwago2 <markmwago@gmail.com>
2019-07-29 10:43:32 +0300

    add internet permissions and okhttp de[endencies

--- app/src/main/java/com/example/readmore/Constants.java
+++ app/src/main/java/com/example/readmore/Constants.java
@@ -1,5 +1,8 @@
 package com.example.readmore;
 
 public class Constants {
-    public static final String GoodReads_Token = BuildConfig.Goodreads_TOKEN;
+    public static final String Goodreads_Token = BuildConfig.Goodreads_TOKEN;
+    public static final String Goodreads_BASE_URL = "https://www.goodreads.com/api/index#search.books";
+    public static final String Goodreads_Books_QUERY_PARAMETER = "location";
+
 }
