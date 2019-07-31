c51df677bb7b5d400fadf99c18fdb86e08d93001
MarkMwago2 <markmwago@gmail.com>
2019-07-31 15:52:27 +0300

    refactor the library activity to display API on recycler view

--- app/src/main/java/com/example/readmore/ui/LibraryActivity.java
+++ app/src/main/java/com/example/readmore/ui/LibraryActivity.java
@@ -8,6 +8,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,6 +21,7 @@ import okhttp3.Callback;
 import okhttp3.Response;
 import java.util.*;
 
+import com.example.readmore.adapters.LibraryListAdapter;
 import com.example.readmore.models.Books;
 import com.example.readmore.R;
 import com.example.readmore.service.ReadService;
@@ -27,12 +30,12 @@ import com.example.readmore.service.ReadService;
 public class LibraryActivity extends AppCompatActivity {
     public static final String TAG = LibraryActivity.class.getSimpleName();
     //    private TextView mbooksTextView;
-    @BindView(R.id.booksTextView)
-    TextView mbooksTextView;
+//    @BindView(R.id.booksTextView) TextView mbooksTextView;
     //    private ListView mListView;
-//    @BindView(R.id.listView)
-//    ListView mListView;
-    private List<Books> books = new ArrayList<>();
+//    @BindView(R.id.listView)ListView mListView;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private LibraryListAdapter mAdapter;
+    private ArrayList<Books> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +50,7 @@ public class LibraryActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("library");
-        mbooksTextView.setText("Here are all the books in the library ");
+//        mbooksTextView.setText("Here are all the books in the library ");
 
     }
 
@@ -72,6 +75,12 @@ public class LibraryActivity extends AppCompatActivity {
                     LibraryActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
+                            mAdapter = new LibraryListAdapter(getApplicationContext(), books);
+                            mRecyclerView.setAdapter(mAdapter);
+                            RecyclerView.LayoutManager layoutManager =
+                                    new LinearLayoutManager(LibraryActivity.this);
+                            mRecyclerView.setLayoutManager(layoutManager);
+                            mRecyclerView.setHasFixedSize(true);
 //                            String[] bookNames = new String[books.size()];
 //                            for (int i = 0; i < bookNames.length; i++) {
 //                                bookNames[i] = books.get(i).getTitle();
