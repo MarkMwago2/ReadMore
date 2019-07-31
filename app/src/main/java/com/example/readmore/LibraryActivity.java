b88ce25f6b46b5380d8781310b782202e6ed4d98
MarkMwago2 <markmwago@gmail.com>
2019-07-31 08:24:07 +0300

    implement threading on the interface

--- app/src/main/java/com/example/readmore/LibraryActivity.java
+++ app/src/main/java/com/example/readmore/LibraryActivity.java
@@ -1,26 +1,24 @@
 package com.example.readmore;
 
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
+import java.util.*;
+import com.example.readmore.Books;
 
 
 public class LibraryActivity extends AppCompatActivity {
@@ -31,8 +29,7 @@ public class LibraryActivity extends AppCompatActivity {
     //    private ListView mListView;
     @BindView(R.id.listView)
     ListView mListView;
-    private String[] books = new String[]{"abstractart", "purple", "azkaban", "gatsby", "flights"};
-//    private String[] images = new String[] {"@drawable/abstractart", "@drawable/purple", "@drawable/azkaban", "@drawable/gatsby", "@drawable/flights"};
+    private List<Books> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,22 +42,10 @@ public class LibraryActivity extends AppCompatActivity {
 //        mListView = (ListView) findViewById(R.id.listView);
 //        mbooksTextView = (TextView) findViewById(R.id.booksTextView);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, books);
-        mListView.setAdapter(adapter);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("library");
+        mbooksTextView.setText("Here are all the books in the library ");
 
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView) view).getText().toString();
-                Toast.makeText(LibraryActivity.this, restaurant, Toast.LENGTH_LONG).show();
-
-                Intent intent = getIntent();
-                String location = intent.getStringExtra("library");
-                mbooksTextView.setText("Here are all the books in the library ");
-
-            }
-        });
     }
 
 //                @Override
@@ -69,25 +54,40 @@ public class LibraryActivity extends AppCompatActivity {
 //                    startActivity(intent);final ListView list = findViewById(R.id.listView);
 
 
-                private void getTitle(String title) {
-                    final ReadService readService = new ReadService();
-                    readService.findTitle(title, new Callback() {
+    private void getTitle(String title) {
+        final ReadService readService = new ReadService();
+        readService.findTitle(title, new Callback() {
 
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+                @Override
+                public void onResponse(Call call, Response response){
+                    books = readService.processResults(response);
+                    LibraryActivity.this.runOnUiThread(new Runnable() {
                         @Override
-                        public void onFailure(Call call, IOException e) {
-                            e.printStackTrace();
-                        }
-                        @Override
-                        public void onResponse(Call call, Response response) throws IOException {
-                            try {
-                                String jsonData = response.body().string();
-                                Log.d(TAG, jsonData);
-                            } catch (IOException e) {
-                                e.printStackTrace();
+                        public void run() {
+                            String[] bookNames = new String[books.size()];
+                            for (int i = 0; i < bookNames.length; i++) {
+                                bookNames[i] = books.get(i).getTitle();
+
+                                ArrayAdapter adapter = new ArrayAdapter(LibraryActivity.this, android.R.layout.simple_list_item_1, bookNames);
+                                mListView.setAdapter(adapter);
+
+                                for (Books book : books) {
+                                    Log.d(TAG, "Title: " + book.getTitle());
+                                    Log.d(TAG, "Author: " + book.getAuthor());
+                                    Log.d(TAG, "Id: " + book.getId());
+
+                                }
+
                             }
                         }
                     });
                 }
+
+        });
+    }
 }
-
-
