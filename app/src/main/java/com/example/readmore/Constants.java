a6b4119248d351b82858439049bfe489a9996f5d
MarkMwago2 <markmwago@gmail.com>
2019-08-08 09:50:37 +0300

    add preference searche key on constants

--- app/src/main/java/com/example/readmore/Constants.java
+++ app/src/main/java/com/example/readmore/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public static final String GOODREADS_BASE_URL = "https://www.goodreads.com/search/index.xml";
     public static final String GOODREADS_BOOKS_QUERY_PARAMETER = "q";
     public static final String GOODREADS_KEY_PARAMETER = "key";
+    public static final String PREFERENCES_BOOKS_KEY = "books";
 
 }
