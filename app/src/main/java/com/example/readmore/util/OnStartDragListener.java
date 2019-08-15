d574ff85be83969c3f8dbdfb9254e1e8c27f158e
MarkMwago2 <markmwago@gmail.com>
2019-08-15 07:34:26 +0300

    create the listener for a drag gesture

---
+++ app/src/main/java/com/example/readmore/util/OnStartDragListener.java
@@ -0,0 +1,8 @@
+package com.example.readmore.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+
+}
